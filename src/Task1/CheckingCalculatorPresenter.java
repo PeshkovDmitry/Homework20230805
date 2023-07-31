2047ae950fa96c291ce35039c18ad6b8274be7c2
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-07-31 19:54:39 +0300

    Добавлена обработка ошибок

---
+++ src/Task1/CheckingCalculatorPresenter.java
@@ -0,0 +1,19 @@
+package Task1;
+
+public class CheckingCalculatorPresenter extends CalculatorPresenter {
+    public CheckingCalculatorPresenter(Model model, View view) {
+        super(model, view);
+    }
+
+    @Override
+    public void onButtonClicked() {
+        try {
+            super.onButtonClicked();
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        Model model = new CalculatorModel();
-        Model model = new CheckingCalculatorModel();
 //        View view = new CalculatorView(new Scanner(System.in));
+//        CalculatorPresenter presenter = new CalculatorPresenter(model, view);
+        Model model = new CheckingCalculatorModel();
         View view = new CheckingCalculatorView(new Scanner(System.in));
-        CalculatorPresenter presenter = new CalculatorPresenter(model, view);
+        CalculatorPresenter presenter = new CheckingCalculatorPresenter(model, view);
         presenter.onButtonClicked();
     }
 }
